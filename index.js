78a3e66284908ddc5690b6f3348cde8edcb6a3e0
आदित्य सिन्हा <adityasinha@aditya-ka-MacBook-Pro.local>
2026-03-16 10:31:31 +0530

    Twinkling stars. No sound

---
+++ index.js
@@ -0,0 +1,22 @@
+const बटन = document.querySelector('button');
+const रात = document.querySelector('#रात');
+
+बटन.addEventListener('click', () => {
+    for (let i = 0; i < 20; i++) {
+        const तारा = document.createElement('div');
+        तारा.classList.add('तारा');
+        तारा.innerText = '✨';
+        तारा.style.top = Math.random() * 100 + "%";
+        तारा.style.left = Math.random() * 100 + "%";
+        तारा.style.animationDuration = getRandomBetween(1, 3) + 's';
+        रात.appendChild(तारा);
+
+        तारा.addEventListener('animationend', () => {
+            तारा.remove();
+        });
+    }
+});
+
+function getRandomBetween(min, max) {
+  return Math.random() * (max - min) + min;
+}
